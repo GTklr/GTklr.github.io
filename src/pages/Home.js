1424c85af3a60501a2546a7da4146eccd52563f4
GTklr <56957480+GTklr@users.noreply.github.com>
2023-02-14 22:25:30 +0800

    edited home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import "./Home.css"
+
+//icon imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 import profPic from "../assets/3-smal.jpg"
 
 export default function Home() {
@@ -11,7 +16,8 @@ export default function Home() {
                     <h2 className="text-start typewriter me-auto">Front-End Developer</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit
                     <br /> Molestiae quibusdam aliquid mollitia quia vitae sit.</p>
-                    <a className="btn btn-outline-light"> Contact </a>
+                    <a className="btn btn-md btn-light me-3 greenBtn">contact</a>
+                    <a className="btn btn-md btn-outline-light greenBtn"><FontAwesomeIcon className="me-2" icon={faGithub} size="1x"/>repo</a>
                 </div>
 
                 <div>
